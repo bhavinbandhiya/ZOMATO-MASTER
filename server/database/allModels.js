4768ca75af86c239596a235b21b0e9eeccd75070
bhavinbandhiya <bhavinbandhiya003@gmail.com>
2021-10-12 15:38:16 +0530

    addAPI

---
+++ server/database/allModels.js
@@ -0,0 +1,17 @@
+import {FoodModel} from "./food";
+import {ImageModel} from "./image";
+import {MenuModel} from "./menu";
+import {OrderModel} from "./order";
+import {RestaurantsModel} from "./restaurants";
+import {ReviewsModel} from "./reviews";
+import {UserModel} from "./user";
+
+export {
+    FoodModel,
+    ImageModel,
+    MenuModel,
+    OrderModel,
+    RestaurantsModel,
+    ReviewsModel,
+    UserModel
+};
